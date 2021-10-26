4588d72c33345b882f875109985cfb47fd5fc64f
B Suresh <80473048+suresh181@users.noreply.github.com>
2021-10-26 08:49:37 +0530

    Add files via upload

---
+++ src/main/java/com/example/implicitintent/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.implicitintent
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        btnImage.setOnClickListener {
+            Intent(Intent.ACTION_GET_CONTENT).also {
+                it.type = "image/*"
+                startActivityForResult(it, 0)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK && resultCode == 0){
+            val uri = data?.data
+            ivphoto.setImageURI(uri)
+
+        }
+    }
+}
